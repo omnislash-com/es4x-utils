4abaec3848f885a8336c88eb798aa6658a9aedd7
Michael Jegat <mike.jegat@gmail.com>
2025-01-29 10:40:45 -0800

    add to title case

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -721,6 +721,11 @@ class	StringUtils
 		return rows; // Return the parsed data Array
 	}
 
+	static	ToTitleCase(_str)
+	{
+		return _str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	}
+
 }
 
 module.exports = {
